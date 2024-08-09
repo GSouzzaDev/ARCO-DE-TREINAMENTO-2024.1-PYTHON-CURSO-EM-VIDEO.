f8d9433dfe212ed20489ece46587734fa625c275
unknown <gustavo69gls@gmail.com>
2024-08-09 19:03:12 -0300

    Tarefas

---
+++ Tarefa (1).py
@@ -0,0 +1,14 @@
+nome=input('Qual o seu nome?')
+print('Bem vindo(a)', nome)
+
+print('Digite a sua data de nascimento')
+dia=input('Dia:')
+mes=input('Mes:')
+ano=input('ano:')
+
+print('voce nasceu no dia', dia, 'no mes', mes, 'do ano', ano, nome)
+
+print('por fim, vamos somar dois numeros')
+n1=int(input('primeiro numero: '))
+n2=int(input('segundo numero: '))
+print('soma entre {} e {} resulta em {}'.format(n1,n2,n1+n2))
